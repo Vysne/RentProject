de1e543c1ef45d5ceaf2083ffcf93de189d03d47
Klaidelio <vysnklaidas@gmail.com>
2021-10-06 17:34:41 +0200

    Example website wireframe

--- index.php
+++ index.php
@@ -3,9 +3,89 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>RentProject</title>
+
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/7a8570d225.js" crossorigin="anonymous"></script>
+
+    <!-- Own Stylesheet -->
+    <!-- <link rel="stylesheet" type="text/css" href="css/styles.css" /> -->
+
+    <!-- New Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="css/new_styles.css" />
 </head>
 <body>
-    <h1>This is a index page</h1>
+    
+    <nav id="side_navbar" class="side_navbar">
+
+        <div id="page_logo" class="logo">
+            <img src="./assets/projectname.png" alt="Logo" width="500" heigth="500">
+        </div>
+
+        
+        <div id="navbar_menu">
+            
+            <ul id="menu_list">
+
+                <li>
+                    <a href='#' class="active_link">
+                       <i class="fas fa-home"></i>
+                       Home 
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <i class="fas fa-search"></i>
+                        Search
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <i class="fas fa-book"></i>
+                        Library
+                    </a>
+                </li>
+                <li>
+                    <a href='#'>
+                        <i class="fas fa-handshake"></i>
+                        Sponsors
+                    </a>
+                </li>
+                <li id="logout_btn">
+                    <a href='#'>
+                        <i class="fas fa-sign-out-alt"></i>
+                        Logout
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+    </nav>
+
+    <header id="page_banner">
+
+        <div id="user_container">
+
+            <div id="user_field">
+
+                <i class="fas fa-user"></i>
+
+                <span id="userName">TestUser</span>
+
+            </div>
+
+
+            <div id="user_details">
+                <ul id="user_list">
+                    <li>Fullname: Test User</li>
+                    <li>Group: Not in group</li>
+                </ul>
+            </div>
+            
+        </div>
+
+    </header>
+
 </body>
 </html>
