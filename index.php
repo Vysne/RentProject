ec35fd9398211ad7eba2a48de184c68242929a89
Klaidelio <vysnklaidas@gmail.com>
2021-10-08 15:25:06 +0200

    Javascript fixes

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 
 <body>
 
-    <div class="wrapper">
+    <div class="wrapper" id="wrapper">
         <!-- Sidebar -->
         <nav id="sidebar">
             <div class="sidebar-header">
@@ -32,7 +32,7 @@
             </div>
             
             <ul class="list-unstyled components">
-                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="javascript:void(0)" class="closebtn" id="closeBtn" onclick="closeNav();">&times;</a>
                 <li class="active">
                     <a href="#">Home</a>
                 <li>
@@ -71,21 +71,21 @@
         </div>-->
 
         <div id="content">
-            <button class="openbtn" onclick="openNav()">&#9776; Menu</button>
+            <button class="openbtn" id='openBtn' onclick="openNav();">&#9776; Menu</button>
         </div>    
 
     </div>
     <!-- Botstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-    <!-- JQuery CDN(without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- JQuery CDN(without AJAX)
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
 
-    <!-- Popper.JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Popper.JS
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script> -->
 
     <!-- Custom JS -->
-    <script type="text/javascript" src="js/javascript.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
 </body>
 
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("wrapper").style.width = "250px";
     document.getElementById("content").style.marginLeft = "250px";
@@ -9,3 +9,30 @@ function openNav() {
     document.getElementById("wrapper").style.width = "0";
     document.getElementById("content").style.marginLeft = "0";
   }
+
+// let wrapper = document.getElementById('wrapper');
+// let content = document.getElementById('content');
+
+// let opneBtn = document.getElementById('openBtn');
+// let closeBtn = document.getElementById('closeBtn');
+
+// opneBtn.addEventListener('click', () => {
+//   openNav(wrapper, content);
+//   console.log('open');
+// });
+
+// closeBtn.addEventListener('click', () => {
+//   closeNav(wrapper, content);
+//   console.log('close');
+// });
+
+
+// function openNav(wrapper, content) {
+//   wrapper.style.width = '250px';
+//   content.style.marginLeft = '250px';
+// }
+
+// function closeNav(wrapper, content) {
+//   wrapper.style.width = '0px';
+//   content.style.marginLeft = '0px';
+// }
