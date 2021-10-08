9e63627ecc91ffd7fc1e8ed27f76420d139ff843
Vysne <vysnius272@gmail.com>
2021-10-08 15:07:58 +0300

    my version of project

--- index.php
+++ index.php
@@ -1,87 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <!-- Own CSS -->
-    <link rel="stylesheet" type="text/css" href="./css/styles.css">
-</head>
-<body>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
     
-    <div class="container-fluid">
+    <title>Eventify</title>
 
-        <div class="row mainRow">
+</head>
 
-            <div class="col-2 sidebar">
+<body>
 
-                <div class="nav-logo">
-                    <a href="index.php" class="nav-brand">
-                        <img src="./assets/projectname.png" class="img-fluid" alt="Logo">
-                    </a>
-                </div>
-
-                <div class="navbar-wrapper">
-
-                    <nav id="sidenavbar"class="navbar">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 text-center pt-5">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Search</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Library</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Sponsors</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Logout</a>
-                            </li>
-                        </ul>
-                    </nav>
-
-                </div>
-
+    <div class="wrapper">
+        <!-- Sidebar -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Evenitfy Menu</h3>
+                <!-- <img src="/assets/projectname.png" alt="Logo" width="300" height="200"> -->
             </div>
+            
+            <ul class="list-unstyled components">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <li class="active">
+                    <a href="#">Home</a>
+                <li>
+                    <a href="#">Search</a>
+                </li>
+                <li>
+                    <a href="#">Library</a>
+                </li>
+                <li>
+                    <a href="#">Sponsors</a>
+                </li>
+                <li>
+                    <a href="#">Places</a>
+                </li>
+            </ul>
+            <div class="logoutbtn" 
+                <ul class="list-unstyled components">
+                    <li>
+                        <a href="#">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 
-
-            <div class="col-10">
-
-                <div id="banner">
-
-                    <div id="user_container">
-
-                        <div class="card text-center" style="width: 18rem;">
-                            <div class="card-body">
-                                <h5 class="cart-title">
-                                    TestUser
-                                </h5>
-                                <button class="btn btn-primary">
-                                    Menu
-                                </button>
-                            </div>
-                        </div>
-
-                    </div>
-
+        <!-- Page Content -->
+        <!-- <div id="content">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Toggle Sidebar</span>
+                    </button>
                 </div>
+            </nav>
+        </div>-->
 
-            </div>  
-
-        </div>  
+        <div id="content">
+            <button class="openbtn" onclick="openNav()">&#9776; Menu</button>
+        </div>    
 
     </div>
-    
+    <!-- Botstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+    <!-- JQuery CDN(without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
+    <!-- Custom JS -->
+    <script type="text/javascript" src="js/javascript.js"></script>
 </body>
+
 </html>
---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("wrapper").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("wrapper").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+  }
