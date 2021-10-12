76a0a14e7be75dfca97725e42af7a08ec9c30261
Vysne <vysnius272@gmail.com>
2021-10-12 14:53:31 +0300

    Created new userinfo buton

--- index.php
+++ index.php
@@ -8,10 +8,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- W3 CSS -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <!-- Font Awesome Kit -->
+    <script src="https://kit.fontawesome.com/7a8570d225.js" crossorigin="anonymous"></script>
 
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
@@ -23,12 +29,15 @@
 
 <body>
 
-    <div id="wrapper">
+    <!-- Custom JS -->
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+    <div class="wrapper">
 
         <nav id="sidebar">
             
             <div class="sidebar-header">
-                <h3>Evenitfy Menu<h3>
+                <img src="assets/projectname.png" alt="Logo" width="200" height="100">
             </div>
 
             <ul class="list-unstyled components">
@@ -47,36 +56,39 @@
                 <li>
                     <a href="#">Places</a>
                 </li>
-                <div class="logoutbtn"> 
+            </ul>
+            <div class="logoutbtn"> 
                     <ul class="list-unstyled components">
                         <li>
                             <a href="#">Log out</a>
                         </li>
                     </ul>
-                </div>
-            </ul>
-
+            </div>
         </nav>
-
+        
         <div id="content">
             <button class="openbtn" id='openBtn' onclick="openNav();">&#9776; Menu</button>
         </div>
 
+        <div id="dropdown">
+            <button class="userbtn" onclick="myFunction()"><i class="fas fa-user-circle"></i> User_Name_Surname <i class="fas fa-caret-down"></i></button>
+            <div id="Demo" class="w3-dropdown-content w3-bar-block w3-border w3-black">
+                <img src="assets/projectname.png" alt="Logo" width="150" height="80"><br><br>
+                <p><b><strong>Band: Band_Name</strong></b></p>
+                <p><b><strong>Popularity: Popularity_Number</strong></b></p>
+            </div>
+        </div>
+
     </div>
 
+</div>
 
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <!-- Botstrap JS -->
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-    <!-- JQuery CDN(without AJAX)
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-
-    <!-- Popper.JS
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script> -->
-
-    <!-- Custom JS -->
-    <script type="text/javascript" src="js/scripts.js"></script>
 </body>
 
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,12 @@ function closeNav() {
   document.getElementById("sidebar").style.marginLeft = "-250px";
   document.getElementById("content").style.marginLeft = "0px";
 }
+
+function myFunction() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) { 
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
