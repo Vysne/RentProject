6b89bdacb6324fb0a563eb042b9aa896e943837f
Vysne <vysnius272@gmail.com>
2021-10-21 01:14:20 +0300

    Cleaned unnecessary css styles, js scripts, added a logo and remade background gradient. Additionaly, made small animation on user button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,4 @@
-// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById('sidebar').style.marginLeft = "0px";
-    document.getElementById("content").style.marginLeft = "-250px";
-  }
-  
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  document.getElementById("sidebar").style.marginLeft = "-250px";
-  document.getElementById("content").style.marginLeft = "0px";
-}
-
-/* W3.CSS dropdown script */
-function DropdownContent() {
-  var x = document.getElementById("Demo");
-  if (x.className.indexOf("w3-show") == -1) { 
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
-
+/* Getting user's local time when opening web and depending on time, the text prints */
 function setGreeting() {
   const time = new Date().getHours();
   let greeting;
